71014e43da922dc637ce464517baf5dff168bd71
tomimarkus991 <tomimarkusalber@gmail.com>
2024-09-18 18:59:41 +0300

    Fix appid

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.onlysignals.vite",
+  appId: "com.onlysignals.www",
   appName: "OnlySignals",
   webDir: "dist",
   server: {
